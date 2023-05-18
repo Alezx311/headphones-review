ed28c6c112d8e89f84161f83355b68884b69e5c1
Oleksii Sokolov <alezx311@gmail.com>
2023-05-18 23:41:18 +0300

    Update dev scripts

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,15 +1,86 @@
-const { readFileSync, exists, existsSync, writeFileSync } = require('fs')
+const { readFileSync, writeFileSync, readdirSync, statSync, existsSync } = require('fs')
 
-const { debug, warn } = console
+const { debug } = console
 const { isArray } = Array
 const { now } = Date
 
-const isExist = v => v !== null && v !== undefined
-const isNumber = v => typeof v === 'number'
-const isString = v => typeof v === 'string'
-const isObject = v => typeof v === 'object' && v !== null && !Array.isArray(v)
-const isLength = v => (isArray(v) || isString(v)) && v.length > 0
-const isDate = v => v instanceof Date
+class Files {
+  static DIR = __dirname
+  static ROOT = resolve(__dirname, '../')
+  static IMGS = resolve(__dirname, '../images')
+  static REFS = resolve(__dirname, '../references')
+
+  static read(file) {
+    const results = { file, content: null }
+    try {
+      if (!existsSync(file)) throw new Error('Not exists!')
+      if (!statSync(file).isFile()) throw new Error('Not file!')
+      results.content = readFileSync(file, { encoding: 'utf-8' }).toString()
+    } catch (e) {
+      results.error = e
+    }
+
+    return results
+  }
+
+  static write(file, data) {
+    try {
+      writeFileSync(file, data, { encoding: 'utf-8' })
+      console.info(`${data.length} chars was saved to ${file}`)
+      return true
+    } catch (e) {
+      console.warn('Error on saving data to file!')
+      console.error(e)
+      return false
+    }
+  }
+
+  static ls(dir) {
+    const results = { dir, dirs: null, files: null }
+    try {
+      const finded = readdirSync(dir, { withFileTypes: true, encoding: 'utf-8' }).filter(Boolean)
+      results.dirs = finded.filter(f => f.isDirectory()).map(f => f.name)
+      results.files = finded.filter(f => f.isFile()).map(f => f.name)
+    } catch (e) {
+      results.error = e
+    }
+
+    return results
+  }
+}
+
+class Logs {
+  static toMessage(text = 'Message', level = 'Log') {
+    return `\n---> ${level} <---\n${text}\n`
+  }
+
+  static toJson(...values) {
+    return JSON.stringify(values, null, '\t')
+  }
+
+  static show(value, level = 'default') {
+    switch (level) {
+      case 'log':
+        console.log(value)
+      case 'debug':
+        console.debug(value)
+      case 'warn':
+        console.warn(value)
+      case 'error':
+        console.error(value)
+      case 'default':
+        console.info(value)
+      default: {
+        if (typeof value === 'object' && value !== null) console.table(value)
+        else console.log(value)
+      }
+    }
+  }
+
+  static debug(value, message = 'Debug Message') {
+    debug({ message: this.toMessage(message), value })
+  }
+}
 
 class Urls {
   static read(file) {
@@ -31,7 +102,7 @@ class Urls {
       (a, s) => [
         ...a,
         `echo "Downloading ${i + 1}/${urls.length}..."`,
-        `curl -o '${s.match(/[^\\\/](.+)$/im)?.[0]}' '${s}'`,
+        `curl -o '${s.match(/[^\\\/](.+)$/im)?.[0]}' '${s}' --max-time 10`,
       ],
       ['#!/bin/sh\n\n'],
     )
@@ -44,9 +115,7 @@ class Urls {
 }
 
 class Time {
-  static DATE_INIT = new Date()
-
-  static MS_INIT = this.DATE_INIT.getTime()
+  static MS_INIT = now()
   static MS_1970 = new Date(0).getTime()
   static MS_2000 = new Date('01/01/2000').getTime()
   static MS_2001 = new Date('01/01/2001').getTime()
@@ -57,81 +126,7 @@ class Time {
   static MS_WEEK = this.MS_DAY * 7
   static MS_YEAR = this.MS_DAY * 365
 
-  static isMs(v) {
-    return isNumber(v) && v > 0
-  }
-
-  static isMsPast(ms) {
-    return this.isMs(ms) && ms < this.ms
-  }
-
-  static isMsFuture(ms) {
-    return this.isMs(ms) && ms > this.ms
-  }
-
-  static isMsActual(ms, diff) {
-    if (!this.isMs(ms)) {
-      return false
-    }
-
-    if (!this.isMs(diff)) {
-      diff = this.MS_HOUR
-    }
-
-    const current = this.ms
-    const min = current - diff
-    const max = current + diff
-    const result = ms > min && ms < max
-
-    if (!result) {
-      warn({ message: 'Provided ms is not actual milliseconds value!', ms, diff, current, min, max })
-    }
-
-    return result
-  }
-
-  static isMsRange(v) {
-    return isArray(v) && v.length === 2 && v.every(isNumber)
-  }
-  static isMsInRange(ms, range) {
-    return ms > Math.min(...range) && ms < Math.max(...range)
-  }
-  static range(ms) {
-    return {
-      past: [0, Date.now()],
-      sec: [ms - MS_SEC, ms + MS_SEC],
-      min: [ms - MS_MIN, ms + MS_MIN],
-      hour: [ms - MS_HOUR, ms + MS_HOUR],
-      day: [ms - MS_DAY, ms + MS_DAY],
-      week: [ms - MS_WEEK, ms + MS_WEEK],
-      year: [ms - MS_YEAR, ms + MS_YEAR],
-    }
-  }
-
-  static get ms() {
-    return Date.now()
-  }
-  static get obj() {
-    return new Date()
-  }
-
-  static toSrc(ms) {
-    if (!this.isMs(ms)) {
-      ms = this.ms
-    }
-
-    return new Date(ms)
-  }
-
-  static isDate(obj) {
-    return obj instanceof Date
-  }
-
   static toExamples(ms = now()) {
-    if (!isNumber(ms)) {
-      ms = Date.now()
-    }
-
     const obj = new Date(ms)
     const results = {
       ms,
@@ -194,12 +189,19 @@ class Time {
   }
 }
 
+class Is {
+  static defined = v => v !== null && v !== undefined
+  static number = v => typeof v === 'number'
+  static string = v => typeof v === 'string'
+  static object = v => typeof v === 'object' && v !== null && !Array.isArray(v)
+  static length = v => (isArray(v) || isString(v)) && v.length > 0
+  static date = v => v instanceof Date
+}
+
 module.exports = {
-  isExist,
-  isNumber,
-  isString,
-  isObject,
-  isLength,
-  isDate,
+  Is,
   Time,
+  Logs,
+  Urls,
+  Files,
 }
